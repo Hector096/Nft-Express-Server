8ce08bd41776e509c6c4b7faf6c2750d7f1c7b3e
Vishal Verma <vishal@genetico.in>
2022-05-15 23:01:45 +0530

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 app.use(express.json());
-
+app.use(cors());
 // Routes
 const indexRoute = require('./routes/index');
 
